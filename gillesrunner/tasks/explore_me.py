4f1c5c1e413cdb24c3c41da0ffa39c12ccb8eaf0
gillesshi <gillesshi@leyinetwork.com>
2021-06-01 22:47:11 +0800

    feat: exit if fail

--- gillesrunner/tasks/explore_me.py
+++ gillesrunner/tasks/explore_me.py
@@ -1,22 +1,33 @@
 # -*- encoding=utf8 -*-
 __author__ = "gillesshi"
 
+import time
+
 from gillescommon.operation.operation_basic import my_exist_and_touch, my_touch
 from gillescommon.position.images import map_switch_me, explore_2_me, explore_3_me, dispatch_me, dispatch_2_me, \
-    dispatch_3_me
-from gillescommon.position.points import post_station_me, explore_me
+    dispatch_3_me, cross_2_me
+from gillescommon.position.points import post_station_me, explore_me, map_switch_2_me
+
+
+def _explore_me():
+    my_touch(map_switch_2_me, wait_time=1)
+    while my_exist_and_touch(map_switch_me, wait_time=1):
+        pass
+    my_touch(post_station_me, wait_time=1)
+    my_touch(explore_me, wait_time=1)
+    if my_exist_and_touch(explore_2_me, wait_time=1):
+        my_exist_and_touch(explore_3_me, wait_time=0.5)
+        for i in [dispatch_me, dispatch_2_me, dispatch_3_me]:
+            if my_exist_and_touch(i, wait_time=0.5):
+                break
+    else:
+        my_touch(cross_2_me)
 
 
 def main():
-    while my_exist_and_touch(map_switch_me, wait_time=1):
-        pass
-    my_touch(post_station_me, wait_time=0.5)
-    my_touch(explore_me, wait_time=0.5)
-    my_exist_and_touch(explore_2_me, wait_time=1)
-    my_exist_and_touch(explore_3_me, wait_time=0.5)
-    for i in [dispatch_me, dispatch_2_me, dispatch_3_me]:
-        if my_exist_and_touch(i, wait_time=0.5):
-            break
+    for i in range(3):
+        _explore_me()
+        time.sleep(3)
 
 
 
